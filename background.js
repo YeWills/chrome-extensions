189fc4726b7da104f999a9cd57717f5e73ce528d
hz-qq2 <601661706@qq.com>
2022-09-09 20:45:24 +0800

    ok

--- background.js
+++ background.js
@@ -1,30 +1,24 @@
 
 var ruleCookieId = 199;//此数字随机写即可；
 
-// var sourcedomain = 'https://www.baidu.com'
-var sourcedomain = 'http://xn.magichznpm.com'
+// var sourceUrl = 'https://www.baidu.com'
+var sourceUrl = 'http://xn.magichznpm.com'
 
-var targetdomains = 'www.axihe.com'
-var targeturl = targetdomains
+var targetDomain = 'www.axihe.com'
 
 
 
 async function getCookieValues(source){
-  const cookies = await chrome.cookies.getAll(
-        // todo 等下试试，是 url 还是 domail 好，最好使用 url，因为要做到精确匹配 一个
+    const cookies = await chrome.cookies.getAll(
         {url:source}
     )
 
     if(!cookies) return;
 
-    console.log('-----', cookies)
-
     // 类似 "name=KJRiG5DUI; girl=beautiful; "
    const cookieValue = cookies.map(({name, value})=>{
         return `${name}=${value};`
     }).join(' ');
-
-    console.log('-----', cookieValue)
 
     return cookieValue;
 }
@@ -33,18 +27,17 @@ async function getCookieValues(source){
 // 更新请求规则
 async function updateRules(){
 
-    if(!sourcedomain || !targeturl) return;
+    // 如果没有sourceUrl  targetDomain值，就不用给请求头设置cookie
+    if(!sourceUrl || !targetDomain) return;
 
-    const cookieval = await getCookieValues(sourcedomain);
+    const cookieval = await getCookieValues(sourceUrl);
 
-    console.log('--////---', cookieval)
-
+    // 如果没有cookieval值，说明不用给请求头设置cookie，那么清除原来的ruleid
     if(!cookieval) {
         chrome.declarativeNetRequest.updateDynamicRules({
-            // 更新前，先删除旧的id规则，否则报错重复id
-        removeRuleIds: [ruleCookieId], 
+          removeRuleIds: [ruleCookieId], 
         }, function(){
-        console.log('卸载cookie')
+          console.log('卸载cookie')
         });
         return;
     }
@@ -67,35 +60,40 @@ async function updateRules(){
                 ]
                 },
                 condition: {
-                    // urlFilter: ["||a.example.com"],
-                    // todo
-                    // urlFilter: `|${targeturl}`,
-                    // regexFilter: 'magichznpm',
-                    domains:[targetdomains],
-                    // urlFilter: 'xn.magichznpm.com',
-                  resourceTypes: [ "xmlhttprequest"],
-                //   resourceTypes: ["main_frame", "script", "xmlhttprequest", 'other'],
-                //   resourceTypes: [ "xmlhttprequest"],
-                // requestMethods: 'rule.matchConfig.methods as any',
-                // regexFilter:undefined,
-                // urlFilter:undefined,
+                    domains:[targetDomain],
                 },
             }
         ],
         }, function(){
-        console.log('zhixing1')
+           console.log('update cookie')
         });
     
     }
-
 
 
 chrome.runtime.onInstalled.addListener(() => {
     updateRules()
 });
 
-chrome.cookies.onChanged.addListener(function(cause,cookie,removed){
-    updateRules()
+chrome.cookies.onChanged.addListener(function({cause,cookie,removed}){
+    console.log(cookie.domain)
+    console.log(cause,cookie,removed)
+    // 只更新与源地址有关的cookie
+    if(sourceUrl.includes(cookie.domain)){
+        updateRules()
+    }
+})
+
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.type === 'updatecookie') {
+        const {sourceUrl, targetDomain } = request;
+        sourceUrl = sourceUrl
+        targetDomain = targetDomain
+        updateRules()
+      }
 })
 
 
@@ -111,12 +109,3 @@ chrome.cookies.onChanged.addListener(function(cause,cookie,removed){
 //         }
 //     }
 // );
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      if (request.type === 'updatecookie') {
-        const {sourcedomain, targeturl } = request;
-        sourcedomain = sourcedomain
-        targeturl = targeturl
-        updateRules()
-      }
-})
