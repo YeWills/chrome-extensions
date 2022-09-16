b92393ac4d8dcd98147e7150ce8a8f0189dacbbd
hz-qq2 <601661706@qq.com>
2022-09-16 17:40:21 +0800

    1

--- background.js
+++ background.js
@@ -1,107 +1,11 @@
 
 var ruleCookieId = 199;//此数字随机写即可；
 
-var sourceUrl = ''
-// var sourceUrl = 'http://xn.magichznpm.com'
-
-// var targetDomain = 'www.axihe.com'
-var targetDomain = ''
-
-// 关闭插件
-function closeEvent(){
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [ruleCookieId], 
-      }, function(){
-        console.log('卸载cookie')
-      });
-}
-
-function getdomain(url){
-    // 例如'www.axihe.com' 请求地址转域名，需求去掉前面的 www ，然后加上 .
-    return `.${url.split('.').slice(1).join('.')}`
-}
-
-async function getCookieValues(source){
-    let cookies = await chrome.cookies.getAll(
-        {url:source}
-    )
-
-    if(!cookies) return;
-
-    // 需要加上原来的cookie
-    const origincookies = await chrome.cookies.getAll(
-        {domain: getdomain(targetDomain)}
-    )
-    if(origincookies?.length){
-        cookies = [...cookies, ...origincookies]
-    }
-
-    // 类似 "name=KJRiG5DUI; girl=beautiful; "
-   const cookieValue = cookies.map(({name, value})=>{
-        return `${name}=${value};`
-    }).join(' ');
-
-    return cookieValue;
-}
-
-
-// 更新请求规则
-async function updateRules(){
-
-    // 如果没有sourceUrl  targetDomain值，就不用给请求头设置cookie
-    if(!sourceUrl || !targetDomain) return;
-
-    const cookieval = await getCookieValues(sourceUrl);
-
-    // 如果没有cookieval值，说明不用给请求头设置cookie，那么清除原来的ruleid
-    if(!cookieval) {
-        closeEvent()
-        return;
-    }
-   
-
-    chrome.declarativeNetRequest.updateDynamicRules({
-            // 更新前，先删除旧的id规则，否则报错重复id
-        removeRuleIds: [ruleCookieId], 
-        addRules: [
-            {
-                id: ruleCookieId,
-                action: {
-                type: 'modifyHeaders',
-                requestHeaders:[
-                    {
-                        header: 'Cookie',
-                        value: cookieval,
-                        operation: 'set',
-                    }
-                ]
-                },
-                condition: {
-                    domains:[targetDomain],
-                },
-            }
-        ],
-        }, function(){
-           console.log('update cookie')
-        });
-    
-    }
-
 
 chrome.runtime.onInstalled.addListener(() => {
     // 默认不开启
     // updateRules()
 });
-
-chrome.cookies.onChanged.addListener(function({cause,cookie,removed}){
-    console.log(cookie.domain)
-    console.log(cause,cookie,removed)
-    // 只更新与源地址有关的cookie
-    if(sourceUrl.includes(cookie.domain)){
-        updateRules()
-    }
-})
-
 
 
 
@@ -113,21 +17,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         // sendResponse 一定要调用，否则报错
         // https://blog.csdn.net/m0_37729058/article/details/89186257
         sendResponse('get msg')
-      }
-})
-
-
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      if (request.type === 'setStatus') {
-        if(request.value ==='close'){
-            closeEvent()
-            sendResponse('close')
-            return
-        }
-        updateRules();
-        sendResponse('open')
-       
       }
 })
 
---
+++ devtools.js
@@ -0,0 +1,179 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function Console() {}
+
+Console.Type = {
+  LOG: 'log',
+  DEBUG: 'debug',
+  INFO: 'info',
+  WARN: 'warn',
+  ERROR: 'error',
+  GROUP: 'group',
+  GROUP_COLLAPSED: 'groupCollapsed',
+  GROUP_END: 'groupEnd',
+};
+
+Console.addMessage = function (type, format, args) {
+  chrome.extension.sendRequest({
+    command: 'sendToConsole',
+    tabId: chrome.devtools.tabId,
+    args: escape(JSON.stringify(Array.prototype.slice.call(arguments, 0))),
+  });
+};
+
+// Generate Console output methods, i.e. Console.log(), Console.debug() etc.
+(function () {
+  const console_types = Object.getOwnPropertyNames(Console.Type);
+  for (let type = 0; type < console_types.length; ++type) {
+    const method_name = Console.Type[console_types[type]];
+    Console[method_name] = Console.addMessage.bind(Console, method_name);
+  }
+})();
+
+function ChromeFirePHP() {}
+
+ChromeFirePHP.handleFirePhpHeaders = function (har_entry) {
+  const response_headers = har_entry.response.headers;
+  const wf_header_map = {};
+  let had_wf_headers = false;
+
+  for (var i = 0; i < response_headers.length; ++i) {
+    const header = response_headers[i];
+    if (/^X-Wf-/.test(header.name)) {
+      wf_header_map[header.name] = header.value;
+      had_wf_headers = true;
+    }
+  }
+
+  const proto_header = wf_header_map['X-Wf-Protocol-1'];
+  if (!had_wf_headers || !this._checkProtoVersion(proto_header)) return;
+
+  const message_objects = this._buildMessageObjects(wf_header_map);
+  message_objects.sort(function (a, b) {
+    const aFile = a.File || '';
+    const bFile = b.File || '';
+    if (aFile !== bFile) return aFile.localeCompare(bFile);
+    const aLine = a.Line !== undefined ? a.Line : -1;
+    const bLine = b.Line !== undefined ? b.Line : -1;
+    return aLine - bLine;
+  });
+
+  const context = { pageRef: har_entry.pageref };
+  for (var i = 0; i < message_objects.length; ++i) this._processLogMessage(message_objects[i], context);
+  if (context.groupStarted) Console.groupEnd();
+};
+
+ChromeFirePHP._processLogMessage = function (message, context) {
+  const meta = message[0];
+  if (!meta) {
+    Console.error('No Meta in FirePHP message');
+    return;
+  }
+
+  const body = message[1];
+  const type = meta.Type;
+  if (!type) {
+    Console.error('No Type for FirePHP message');
+    return;
+  }
+
+  switch (type) {
+    case 'LOG':
+    case 'INFO':
+    case 'WARN':
+    case 'ERROR':
+      if (!context.groupStarted) {
+        context.groupStarted = true;
+        Console.groupCollapsed(context.pageRef || '');
+      }
+      Console.addMessage(Console.Type[type], '%s%o', meta.Label ? `${meta.Label}: ` : '', body);
+      break;
+    case 'EXCEPTION':
+    case 'TABLE':
+    case 'TRACE':
+    case 'GROUP_START':
+    case 'GROUP_END':
+      // FIXME: implement
+      break;
+  }
+};
+
+ChromeFirePHP._buildMessageObjects = function (header_map) {
+  const normal_header_prefix = 'X-Wf-1-1-1-';
+
+  return this._collectMessageObjectsForPrefix(header_map, normal_header_prefix);
+};
+
+ChromeFirePHP._collectMessageObjectsForPrefix = function (header_map, prefix) {
+  const results = [];
+  const header_regexp = /(?:\d+)?\|(.+)/;
+  let json = '';
+  for (let i = 1; ; ++i) {
+    const name = prefix + i;
+    const value = header_map[name];
+    if (!value) break;
+
+    const match = value.match(header_regexp);
+    if (!match) {
+      Console.error(`Failed to parse FirePHP log message: ${value}`);
+      break;
+    }
+    const json_part = match[1];
+    json += json_part.substring(0, json_part.lastIndexOf('|'));
+    if (json_part.charAt(json_part.length - 1) === '\\') continue;
+    try {
+      const message = JSON.parse(json);
+      results.push(message);
+    } catch (e) {
+      Console.error(`Failed to parse FirePHP log message: ${json}`);
+    }
+    json = '';
+  }
+  return results;
+};
+
+ChromeFirePHP._checkProtoVersion = function (proto_header) {
+  if (!proto_header) {
+    Console.warn('WildFire protocol header not found');
+    return;
+  }
+
+  const match = /http:\/\/meta\.wildfirehq\.org\/Protocol\/([^\/]+)\/(.+)/.exec(proto_header);
+  if (!match) {
+    Console.warn('Invalid WildFire protocol header');
+    return;
+  }
+  const proto_name = match[1];
+  const proto_version = match[2];
+  if (proto_name !== 'JsonStream' || proto_version !== '0.2') {
+    Console.warn('Unknown FirePHP protocol version: %s (expecting JsonStream/0.2)', `${proto_name}/${proto_version}`);
+    return false;
+  }
+  return true;
+};
+
+chrome.devtools.network.addRequestHeaders({
+  'X-FirePHP-Version': '0.0.6',
+});
+
+chrome.devtools.network.getHAR(function (result) {
+  const { entries } = result;
+  if (!entries.length) {
+    Console.warn('ChromeFirePHP suggests that you reload the page to track' + ' FirePHP messages for all the requests');
+  }
+  for (let i = 0; i < entries.length; ++i) ChromeFirePHP.handleFirePhp_headers(entries[i]);
+
+  chrome.devtools.network.onRequestFinished.addListener(ChromeFirePHP.handleFirePhpHeaders.bind(ChromeFirePHP));
+});
+
+
+
+chrome.devtools.panels.create('MyPanel', 'images/icon.png', 'panel/panel.html',function(panel){
+    console.log(panel);
+})
+
+var back
+
+
