c9d56cddb9653013f991776a19a416a1d78a5e49
hz-qq2 <601661706@qq.com>
2022-09-20 18:25:28 +0800

    获取接口请求响应数据

--- background.js
+++ background.js
@@ -10,16 +10,41 @@ chrome.runtime.onInstalled.addListener(() => {
 
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      if (request.type === 'updatecookie') {
-        sourceUrl = request.sourceUrl
-        targetDomain = request.targetDomain
-        updateRules()
-        // sendResponse 一定要调用，否则报错
-        // https://blog.csdn.net/m0_37729058/article/details/89186257
-        sendResponse('get msg')
-      }
+  if (request.type === 'setStatus11') {
+    console.log('setStatus11 ....')
+    console.log(request.data)
+    console.log(request.encoding)
+    sendResponse(1)
+  }
+  if (request.type === 'setStatus116') {
+    console.log('setStatus116 ....')
+    console.log(request.data)
+    console.log(request.encoding)
+    sendResponse(1)
+  }
+  if (request.type === 'setStatus112') {
+    console.log('setStatus112 ....')
+    console.log(request.value)
+    console.log(request.data)
+    sendResponse(1)
+  }
+  if (request.type === 'setStatus1134') {
+    console.log('setStatus1134 ....')
+    console.log(request.value)
+    sendResponse(1)
+  }
 })
 
+
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.type === 'setStatus112') {
+//     console.log('setStatus11 ....')
+//     console.log(request.data)
+//     console.log(request.encoding)
+//     sendResponse(1)
+//   }
+// })
 
 
 
--- devtools.js
+++ devtools.js
@@ -1,16 +1,37 @@
 
 
-chrome.devtools.panels.create('MyPanel', 'images/icon.png', 'panel/panel.html',function(panel){
-    console.log(panel);
-})
+chrome.devtools.network.onNavigated.addListener(function(uuu){
+  chrome.runtime.sendMessage(
+    {
+        type: 'setStatus1134',
+        value: uuu, 
+    },
+    (res) => {
+      console.log('ok')
+    }
+);
 
-var backconnect = chrome.runtime.connect(({
-  name:'devtools-page'
-}))
+});
+
 
 chrome.devtools.network.onRequestFinished.addListener(function(request){
   request.getContent(function(content, encoding){
-    if(request.request.url === 'https://www.baidu.com'){
+    console.log('----')
+    var arr = ['validateCurrentSession', 'doJudgeSupplierUser', 'isSignIn', 'isExistByPhone'];
+    var issame = arr.find(t=> request.request.url.includes(t))
+    if(issame){
+      chrome.runtime.sendMessage(
+        {
+            type: 'setStatus11',
+            value: content, 
+            data: content, 
+            encoding: request.request, 
+        },
+        (res) => {
+          console.log('ok')
+        }
+    );
+
       chrome.runtime.sendMessage({
         name:'panel',
         tabId: chrome.devtools.inspectedWindow.tabId,
@@ -18,7 +39,4 @@ chrome.devtools.network.onRequestFinished.addListener(function(request){
       })
     }
   })
-})
-
-
-
+});
